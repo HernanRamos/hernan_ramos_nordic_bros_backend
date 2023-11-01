6b1866e79f7221b6abbe10af1fa999cf88bb467d
Hernan Ramos <hernan_a.ramos@hotmail.com>
2023-11-01 15:35:32 -0300

    7to Git

--- src/app.js
+++ src/app.js
@@ -4,8 +4,7 @@ const app = express();
 const productManager = new ProductManager();
 
 
-app.get("/", async (req,res) => {
-    res.json("⚔️ BIENVENIDO A NORDIC BROS ⚔️")})
+app.get("/", async (req,res) => {res.json("⚔️ BIENVENIDO A NORDIC BROS ⚔️")})
 
     
 app.get("/products", async (req,res) => {
@@ -14,16 +13,18 @@ app.get("/products", async (req,res) => {
     if (limit) {
         const limited_products = [];
         for (let i = 0; i < limit && i < products.length; i++) {limited_products.push(products[i]);}
-        res.json({ products: limited_products });} 
-    else {res.json({ products });}})
+        res.send(limited_products.map(JSON.stringify).join('\n'));}
+        else {res.send(products.map(JSON.stringify).join('\n'));}});
 
 
 app.get("/products/:productId", async (req,res) => {
     const productId = req.params.productId;
-        const products = await productManager.readProducts();
-        const product = products.find(p => Number(p.id) === Number(productId));
-        if (product) {res.json({ product });}
-        else{res.json({ error: "⛔ PRODUCTO NO ENCONTRADO ⛔" });}})
+    const products = await productManager.readProducts();
+    const product = products.find(p => Number(p.id) === Number(productId));
+    if (product) {res.send({ product });}
+    else{res.json({ error: "⛔ PRODUCTO NO ENCONTRADO ⛔" });}})
+
+
 
         
 app.listen(8080, () => {console.log("SERVIDOR FUCIONANDO ✅");})
